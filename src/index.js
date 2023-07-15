9db76bfe215999721112aed861bd09115ea6f3f4
kesavindia <kesavindia83@gmail.com>
2023-07-15 17:31:52 +0530

    done

--- src/index.js
+++ src/index.js
@@ -4,6 +4,13 @@ const reader = async (fileName) => {
   // write code here
 	// dont change function name
   
+  try{
+    return await fs.readFile(filename,"utf-8");
+  }
+  catch(error){
+    console.error("Error reading file:",error);
+    throw error;
+  }
 };
 
 
